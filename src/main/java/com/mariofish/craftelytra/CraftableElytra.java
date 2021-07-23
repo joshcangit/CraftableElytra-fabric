c59c88d93c3cc2929079931808bd84f0d08db017
joshcangit <yjoshua59@gmail.com>
2021-07-23 19:13:12 +0800

    Files for fabric version of Craftable Elytra

---
+++ src/main/java/com/mariofish/craftelytra/CraftableElytra.java
@@ -0,0 +1,27 @@
+package com.mariofish.craftelytra;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CraftableElytra implements ModInitializer
+{
+
+    public static final String MODID = "craftelytra";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+    public static CraftableElytra INSTANCE;
+    public static final Item ELYTRON = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+
+    @Override
+    public void onInitialize()
+    {
+        INSTANCE = this;
+        Registry.register(Registry.ITEM, new Identifier(MODID, "elytron"), ELYTRON);
+        LOGGER.info("Craftable Elytra setup complete!");
+    }
+}
